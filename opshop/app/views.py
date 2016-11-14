f7aa82cfd77607bd619083dcd53f2defbc75e968
math2k <m@th2k.net>
2016-11-14 20:54:42 +0100

    Better messages

--- opshop/app/views.py
+++ opshop/app/views.py
@@ -24,9 +24,17 @@ class SaleFormView(CreateView):
         if self.object.payment_mode == 'credit':
             t = Transaction(user=self.object.user, amount=-self.object.total)
             t.save()
-            messages.success(self.request, "{0}€ have been substracted from your total. You now have {1}€ available".format(self.object.total, self.object.user.profile.balance))
+            if self.object.user.profile.balance > 0:
+                messages.success(self.request,
+                                 "{0}€ have been substracted from your total. You now have {1}€ available".format(
+                                     self.object.total, self.object.user.profile.balance))
+            else:
+                messages.warning(self.request,
+                                 "{0}€ have been substracted from your total. You balance is negative! ({1}€)".format(
+                                     self.object.total, self.object.user.profile.balance))
         else:
-            messages.success(self.request, "Please insert {0}€ in het potje, we are watching you! :D".format(self.object.total))
+            messages.success(self.request,
+                             "Please insert {0}€ in het potje, we are watching you! :D".format(self.object.total))
         return ret
 
     def get_success_url(self):
